9caed0a15fd59d9676218822e460071a9fb99be4
Afganli97 <113414023+Afganli97@users.noreply.github.com>
2026-06-01 16:18:27 +0400

    Create bsc.py

---
+++ bot/networks/bsc.py
@@ -0,0 +1,17 @@
+"""
+Сеть BSC.
+"""
+import logging
+from .ethereum import EthereumNetwork
+from bot.api_clients import EVMExplorerClient
+
+logger = logging.getLogger(__name__)
+
+class BscNetwork(EthereumNetwork):
+    def __init__(self, network_config: dict, session, explorer_client: EVMExplorerClient):
+        super().__init__(network_config, session, explorer_client)
+        self.weth = network_config["weth"].lower()
+
+    async def validate_address(self, address: str) -> bool:
+        from web3 import Web3
+        return Web3.is_address(address)
